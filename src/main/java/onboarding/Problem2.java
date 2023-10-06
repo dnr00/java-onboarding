bb7b715cefae80edb4f8372b7764fb17f08c28a3
dnr00 <dnr00@naver.com>
2023-10-06 16:52:17 +0900

    1212

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        String answer = " ";
+
+         {
+            cryptogram.replaceAll(".{2}", "");
+        }
+
+        answer = cryptogram;
+        String result = answer;
+
+        return result;
     }
 }
+
+    // if(cryptogram.length() >= 1 && cryptogram.length() <= 1000) //필요한가?
